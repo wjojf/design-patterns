7dc0ec7eca614fb59155598a7d46532c2f13ed0d
Tikhon Kozlov <STPKBFF@yandex.ru>
2022-09-19 00:25:20 +0300

    solid_isp added

---
+++ solid_isp.py
@@ -0,0 +1,39 @@
+from abc import abstractmethod
+
+# Invalid because of: child model may not support fax/scan functions but still will be able to call
+class Machine:
+    def print(self, document):
+        raise NotImplementedError
+
+    def fax(self, document):
+        raise NotImplementedError
+
+    def scan(self, document):
+        raise NotImplementedError
+
+class Printer:
+    @abstractmethod
+    def print(self, document):
+        pass 
+
+class Scanner:
+    @abstractmethod
+    def scan(self, document):
+        pass 
+
+
+class MultiFunctionMachine(Printer, Scanner):
+    @abstractmethod
+    def print(self, document):
+        pass 
+
+    @abstractmethod
+    def scan(self, document):
+        pass 
+
+class PrinScanner(MultiFunctionMachine):
+    def print(self, document):
+        '''do something exact'''
+        
+    def scan(self, document):
+        '''do something exact '''
